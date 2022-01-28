ae50c954c332a1cac78f51f3050b40b65e5c54b6
Seva Konyakhin <sevakonyakhin@gmail.com>
2022-01-28 22:14:01 +0300

    update database connection

--- database.js
+++ database.js
@@ -1,10 +1,22 @@
-const Pool = require('pg').Pool
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//   user: 'me',
+//   host: 'localhost',
+//   database: 'city',
+//   password: 'password',
+//   port: 5432,
+// })
+
+const Pool = require("pg").Pool;
+require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'city',
-  password: 'password',
-  port: 5432,
-})
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    // ssl: {
+    //     rejectUnauthorized: false,
+    // },
+});
 
 module.exports = pool
